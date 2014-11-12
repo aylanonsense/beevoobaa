c1c3a78d0fe4fddd0a7b55b331917cbd217ed619
Ben Myers <bmyers@appnexus.com>
2014-11-11 23:31:22 -0500

    Simpler syntax for defining Constants

--- javascripts/client/Constants.js
+++ javascripts/client/Constants.js
@@ -1,10 +1,6 @@
 if (typeof define !== 'function') { var define = require('amdefine')(module); }
-define([
-], function(
-) {
-	return {
-		TARGET_FRAMES_PER_SECOND: 60,
-		CANVAS_WIDTH: 800,
-		CANVAS_HEIGHT: 600
-	};
+define({
+	TARGET_FRAMES_PER_SECOND: 60,
+	CANVAS_WIDTH: 800,
+	CANVAS_HEIGHT: 600
 });
--- javascripts/shared/Constants.js
+++ javascripts/shared/Constants.js
@@ -1,8 +1,4 @@
 if (typeof define !== 'function') { var define = require('amdefine')(module); }
-define([
-], function(
-) {
-	return {
-		SERVER_UPDATES_PER_SECOND: 4
-	};
+define({
+	SERVER_UPDATES_PER_SECOND: 4
 });
